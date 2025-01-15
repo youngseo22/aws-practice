bb5d9653bab0a3322e796886caf1827c5ec5c1d1
youngseo22 <ysnoh22@sookmyung.ac.kr>
2025-01-15 21:37:22 +0900

    그레이들 수정

--- awsDemo/src/main/java/com/example/awsDemo/AwsDemoApplication.java
+++ awsDemo/src/main/java/com/example/awsDemo/AwsDemoApplication.java
@@ -2,9 +2,10 @@ package com.example.awsDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AwsDemoApplication {
+public class AwsDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsDemoApplication.class, args);
--- awsDemo/src/test/java/com/example/awsDemo/AwsDemoApplicationTests.java
+++ awsDemo/src/test/java/com/example/awsDemo/AwsDemoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AwsDemoApplicationTests {
+class AwsDemoApplicationTests{
 
 	@Test
 	void contextLoads() {
